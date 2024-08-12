95831b52e1c00dd9f7e9b7839a4e4a4a519f8f28
Yuan Ji <yuan.ji@jiwhiz.com>
2024-08-12 16:28:50 -0600

    Fix build

--- backend-api/src/main/java/com/jiwhiz/demo/AiChatbotDemoApplication.java
+++ backend-api/src/main/java/com/jiwhiz/demo/AiChatbotDemoApplication.java
@@ -2,8 +2,15 @@ package com.jiwhiz.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.jiwhiz.demo.common.ApplicationProperties;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ApplicationProperties.class})
+@Slf4j
 public class AiChatbotDemoApplication {
 
     public static void main(String[] args) {
--- backend-api/src/test/java/com/jiwhiz/demo/AiChatbotDemoApplicationTests.java
+++ backend-api/src/test/java/com/jiwhiz/demo/AiChatbotDemoApplicationTests.java
@@ -3,9 +3,11 @@ package com.jiwhiz.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class AiChatbotDemoApplicationTests {
 
 	@Test
