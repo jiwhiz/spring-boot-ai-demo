2535c861969b2a651ac08d7fbb1a932caeb4b78e
Yuan Ji <yuan.ji@jiwhiz.com>
2024-08-13 15:16:53 -0600

    Add home page with chat ui

--- frontend-web/src/app/app-routing.module.ts
+++ frontend-web/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/page/home.component';
 import { LoginComponent } from './login/page/login.component';
 import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
   {
     path: 'login',
     component: LoginComponent
